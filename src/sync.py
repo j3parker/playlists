03930b15d5844611330a700ec5a94a0c0737ce22
Jacob Parker <jacob@solidangle.ca>
2020-01-28 22:57:19 -0500

    Fix yaml and Dockerfile

--- src/sync.py
+++ src/sync.py
@@ -1,1 +1,11 @@
-print('hey')
+import os
+import pathlib
+import yaml
+
+def process_yaml_file(f):
+    doc = yaml.load(f, Loader=yaml.SafeLoader)
+    print(doc)
+
+for path in pathlib.Path('.').glob('*.yaml'):
+    with open(path) as f:
+        process_yaml_file(f)
