a6b3f845880a4d460c5118b5a9c658da57c216f2
Jacob Parker <jacob@solidangle.ca>
2020-01-29 08:34:34 -0500

    Fix typo

--- src/youtube.py
+++ src/youtube.py
@@ -76,7 +76,7 @@ class Client:
             )
 
         elif isinstance(op, model.OpAddToPlaylist):
-            if isinstance(op.id, op.PlaceholderId):
+            if isinstance(op, op.PlaceholderId):
                 playlist_id = self.placeholder_map[op.playlist_id.nonce]
             else:
                 playlist_id = op.playlist_id
