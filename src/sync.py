4d6c2ad5e96b5173b3ce4d4ac86f5af4e4cdfd3d
Jacob Parker <jacob@solidangle.ca>
2020-01-28 23:03:49 -0500

    Tidy up sync.py

--- src/sync.py
+++ src/sync.py
@@ -2,10 +2,14 @@ import os
 import pathlib
 import yaml
 
-def process_yaml_file(f):
-    doc = yaml.load(f, Loader=yaml.SafeLoader)
+def process_yaml_file(doc):
     print(doc)
 
-for path in pathlib.Path('.').glob('*.yaml'):
-    with open(path) as f:
-        process_yaml_file(f)
+def main():
+    for path in pathlib.Path('.').glob('*.yaml'):
+        with open(path) as f:
+            doc = yaml.load(f, Loader=yaml.SafeLoader)
+            process_yaml_file(doc)
+
+if __name__ == "__main__":
+    main()
